19d7239959483e0f4620e69f65eab73c6a3b422c
yazok8 <yazankhirfan@gmail.com>
2023-02-01 20:46:13 -0500

    fix client path in server file

--- server.js
+++ server.js
@@ -39,11 +39,15 @@ app.get('/api/config/paypal', (req, res) =>
 if (process.env.NODE_ENV === 'production') {
   
   //set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('/client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  })
+}else{
+  app.get('/', (req, res)=>{
+    res.send('API is running')
+  })
 }
 
 // //Error NotFound
